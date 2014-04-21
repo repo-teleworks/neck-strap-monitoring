c8f8acc7daedd1504af2ce6f152faa9fdedf98ca
tw-shpark <shpark@teleworks.co.kr>
2014-04-21 11:31:31 +0900

    add some features ( showing worker in each sites, server-side positioning algorithm, timestamp)

--- app.js
+++ app.js
@@ -38,12 +38,14 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
+setInterval(sendParcel, 1000);
 
 http.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
 socket.sockets.on('connection', function(socket){
+    //socket.join(data.data.channel);
     socket.on('developer', function(data){
         if(data.type == 'command'){
             if(data.data.command == 'join') {
@@ -55,6 +57,12 @@ socket.sockets.on('connection', function(socket){
             }
         }
     });
+    socket.on('monitoring', function(data){
+        if(data.type == 'command'){
+            socket.emit('monitoring', {type: 'command', data: 'ACK'});
+            console.log('monitoring client is connected.');
+        }
+    });
 });
 
 var serialPort = new SerialPort("/dev/tty.SLAB_USBtoUART", {
@@ -64,23 +72,88 @@ var serialPort = new SerialPort("/dev/tty.SLAB_USBtoUART", {
 serialPort.open(function () {
     console.log('open');
 
-
     serialPort.on('data', function(data) {
         // Dump raw serial ...
         socket.sockets.in('raw-serial').emit('developer', {type: 'serialDump', data: data.toString()});
 
         // Serial Data parsing
         var serialInput = data.toString();
-        var protocolMatch = serialInput.match(/^BB.AA.(\w*).(\w*).00.FF.FF.00.00.00.00.00.(\w*).CC.DD/);
+        var protocolMatch = serialInput.match(/^BB.AA.(\w*).(\w*).00.FF.FF.(\w*).(\w*).(\w*).00.(\w*).(\w*).CC.DD/);
 
         var payload = {
             sequence: protocolMatch[1],
             number: protocolMatch[2],
-            checksum: protocolMatch[3]
+            sosSignal: protocolMatch[3],
+            strapSignal: protocolMatch[4],
+            srcNode: protocolMatch[5],
+            srcRssi: protocolMatch[6],
+            checksum: protocolMatch[7]
         };
 
         // Dump parsed data info.
         socket.sockets.in('parsing-serial').emit('developer', {type: 'serialParsingState', data: payload});
-        //console.log(payload);
+
+        if(!persons[payload.number]) persons[payload.number] = new aPerson(payload.number);
+        persons[payload.number].newData(payload);
     });
 });
+
+function sendParcel() {
+    /*
+     * SOT - Start of Table
+     * EOT - End   of Table
+     */
+    socket.sockets.emit('monitoring', {type: 'data', data: 'SOT'});
+    for(var i in persons){
+        socket.sockets.emit('monitoring', {type: 'data', data: persons[i]});
+    }
+    socket.sockets.emit('monitoring', {type: 'data', data: 'EOT'});
+
+    console.log('periodic transfer : person list and status are transferred.');
+}
+
+/*
+ * 전체 그룹 리스트
+ */
+var persons = new Array();
+
+/*
+ * 사람 한 명당 object
+ *
+ * - number : 고유 식별번호
+ * - srcNode : 현재 속해 있는 중계기 번호
+ * - srcRssi : 현재 속해 있는 중계기로부터 마지막으로 받은 RSSI Indicator
+ *
+ * - methode :
+ *   - newData(payload) : 새로운 payload 입력에 대한 처리
+ */
+function aPerson(index) {
+    this.number = index;
+    this.srcNode = 0xFF;
+    this.srcRssi = 0xFF;
+    this.neckStrap = 0xFF;
+    this.lastUpdated = new Date().getTime() + 32400000; // add + 9:00 (KST)
+}
+
+aPerson.prototype.newData = function(payload){
+    //this.number = payload.number;
+    if(payload.srcNode != 0) {
+        if(this.srcRssi > payload.srcRssi) {
+            /*
+             * 장소 변경 발생 시,
+             * RSSI 최신으로 업데이트
+             * 현재 소속 중계기를 변경
+             */
+            this.srcRssi = payload.srcRssi;
+            this.srcNode = payload.srcNode;
+        } else {
+            /*
+             * 장소 변경이 없을 경우,
+             * 현재 노드의 RSSI만을 업데이트
+             */
+            this.srcRssi = payload.srcRssi;
+        }
+        this.lastUpdated = new Date().getTime() + 32400000; // add + 9:00 (KST)
+    }
+    this.neckStrap = payload.strapSignal;
+};
--- public/javascripts/neckstrap.js
+++ public/javascripts/neckstrap.js
@@ -2,12 +2,135 @@
  * 안전모 jQuery / Ajax script for client browser
  */
 
-var serverUrl = 'http://192.168.1.81:8080';
+var serverUrl = ':8080';
 var socket = io.connect(serverUrl);
 
-$(document).ready(function() {
+var memberList = [];
 
+/*
+ * View mode = ALL, A, B, C
+ */
+var viewMode = 'ALL';
+
+function modeChange(mode) {
+    viewMode = mode;
+    $('#memberListTable').html("");
+    printMemberList(memberList, mode);
+
+    switch(viewMode){
+        case 'ALL': $("#pageTitle").html("전부 보기");
+            break;
+        case 'A': $("#pageTitle").html("건설현장 A");
+            break;
+        case 'B': $("#pageTitle").html("건설현장 B");
+            break;
+        case 'C': $("#pageTitle").html("건설현장 C");
+            break;
+    }
+}
+
+$(document).ready(function() {
+    socket.emit('monitoring', {type:'command', data:'REQ'});
+    socket.on('monitoring', onReceiveMonitoring);
 });
+
+function onReceiveMonitoring(data){
+    if(data.type == 'data') {
+        if(data.data == 'SOT') {
+            /*
+             * refresh 전송 시
+             * 리스트를 초기화 한다.
+             */
+            memberList = [];
+        } else if(data.data == 'EOT'){
+            /*
+             * End of Table
+             * 테이블을 새로 갱신한다.
+             */
+            $('#memberListTable').html("");
+            printMemberList(memberList, viewMode);
+        } else {
+            /*
+             * Member 데이터 수신
+             * Array에 push
+             */
+            memberList.push(data.data);
+        }
+    }
+}
+
+function printMemberList(memberList, mode){
+    if(mode == 'ALL'){
+        for(var i in memberList){
+            printEachMember(memberList[i]);
+        }
+    } else {
+        for(var i in memberList){
+            if(mode == getMemberLocation(memberList[i].srcNode)) {
+                printEachMember(memberList[i]);
+            }
+        }
+    }
+}
+
+function printEachMember(aMember){
+    $('#memberListTable').append("<tr>" +
+        "<td>" + aMember.number + "</td>" +
+        "<td>" + getMemberName(aMember.number) + "</td>" +
+        "<td>" + setDecorationMemberLocation(getMemberLocation(aMember.srcNode)) + "</td>" +
+        "<td>" + setDecorationNeckStrap(aMember.neckStrap) + "</td>" +
+        "<td>" + setDecorationLastUpdate(aMember.lastUpdated) + "</td>" +
+        "</tr>");
+}
+
+function getMemberName(number) {
+    if(number == '05') return '조종식';
+    else if(number == '06') return '서영광';
+    else if(number == '07') return '홍건수';
+    else if(number == '08') return '박상현';
+    else if(number == '09') return '유인숙';
+    else return 'Unknown';
+}
+
+function getMemberLocation(srcNode) {
+    if(srcNode == '02') return 'A';
+    if(srcNode == '03') return 'B';
+    if(srcNode == '04') return 'C';
+    if(srcNode == 255) return 255;
+}
+
+function setDecorationMemberLocation(srcNode){
+    if(srcNode == 'A') return '<span style="color:green;">A</span>';
+    if(srcNode == 'B') return '<span style="color:blue;">B</span>';
+    if(srcNode == 'C') return '<span style="color:cadetblue;">C</span>';
+    if(srcNode == 255) return '<span style="color:red;">Unknown</span>';
+}
+
+function setDecorationLastUpdate(lastUpdated) {
+    var lastUpdatedDeco = new Date(lastUpdated);
+    var currntTime = new Date();
+    var warningLevel = '<span style="color:black;">';
+    if (currntTime-lastUpdatedDeco > 30 * 1000) {
+        warningLevel = '<span style="color:red;">';
+    } else if (currntTime-lastUpdatedDeco > 15 * 1000) {
+        warningLevel = '<span style="color:darkred;">';
+    }
+
+    return warningLevel +
+        lastUpdatedDeco.toISOString().replace(/T/, ' ').replace(/\..+/, '') +
+        "</span>";
+
+}
+
+function setDecorationNeckStrap(neckStrap) {
+    if (neckStrap == '01') {
+        return '<span class="glyphicon glyphicon-ok strap-yes"></span>'
+    } else if (neckStrap == '00'){
+        return '<span class="glyphicon glyphicon-remove strap-no"></span>'
+    } else {
+        return '<span class="glyphicon glyphicon-question-sign strap-no"></span>'
+    }
+}
 
 function onReceiveDeveloper(data){
     if(data.type == 'command') {
@@ -73,7 +196,8 @@ $("#scrollLockParsing").click( function(){
 
 function onParsingDump(data){
     $("#parsingDumpContents").append("<li> Seq: " + data.sequence + " | Number: " +
-        data.number + " | CheckSum: " + data.checksum + "</li>");
+        data.number + " | CheckSum: " + data.checksum + " | From : " + data.srcNode +
+        " | RSSI : " + data.srcRssi + "</li>");
     setParsingScrollLock();
 }
 
